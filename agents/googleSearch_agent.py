7e8874ad5480907f394554c3bc73ab7c6071aaae
Espérance Codjo AYIWAHOUN <eayiwahoun@gmail.com>
2024-10-11 10:55:50 +0100

    Agent for google research is ok

--- agents/googleSearch_agent.py
+++ agents/googleSearch_agent.py
@@ -2,24 +2,19 @@ import os
 import serpapi
 from base_agent import Agent
 
-client = serpapi.Client(api_key=os.getenv("API_KEY"))
-results = client.search({
-    'engine': 'bing',
-    'q': 'coffee',
-})
-
 
 class GoogleSearchAgent(Agent):
-    def __init__(self, api_key=None):
+    def __init__(self, name=None , api_key=None):
         
         try : 
             self.client = serpapi.Client(api_key=os.getenv("API_KEY") or api_key)
         except Exception as e:
-            raise e
-        #     if not self.api_key:
-        #     raise ValueError("Clé API SerpAPI manquante. Veuillez fournir une clé ou définir 'SERP_API_KEY' dans les variables d'environnement.")
+            raise ValueError("Clé API SerpAPI manquante. Veuillez fournir une clé ou définir 'SERP_API_KEY' dans les variables d'environnement.")
         
-        super().__init__("GoogleSearchAgent")
+        if name:
+            super().__init__(name)
+        else :
+            super().__init__("GoogleSearchAgent")
     
     def description(self):
         return "Effectue des recherches Google avancées via SerpAPI."
@@ -29,28 +24,31 @@ class GoogleSearchAgent(Agent):
             return {"error": "Aucune requête de recherche n'a été fournie."}
 
         try:
-            search_params = {"q": query, 
-                             "num": 10 }
+            search_params = {"q": query }
             
-            results = self.client.search(search_params)
-            print(results)
-            return 
+            results = self.client.search(search_params)["inline_images"]
+            # results # Retourne une liste
+            data = []
+            for result in results:
+                tmp = "Title : \"{}\", Source_name : \"{}\", source_link : \"{}\"".format(result["title"], result["source_name"], result["source"])
+                data.append(tmp)
+                
+                # print(tmp, end="\n\n")
+                
+                if len(data) >= 5:
+                    break
+            
+
+
+
+            
+            return data
         
-            google_results = []
-            if "organic_results" in results:
-                for result in results["organic_results"]:
-                    google_results.append({
-                        "title": result.get("title"),
-                        "link": result.get("link"),
-                        "snippet": result.get("snippet")
-                    })
-
-            return {"query": query, "results": google_results}
-
         except Exception as e:
             return {"error": f"Erreur lors de la recherche Google avec SerpAPI : {str(e)}"}
 
 if __name__ == '__main__':
+    import json
     test = GoogleSearchAgent(api_key="d615b5a79c503a1cd8a52d9c319e334828fcabd85c5fc67c7814a94bd3069445")
     answer = test.run("Benjamin Tardy")
     print(answer)
--- agents/linkedin_agent.py
+++ agents/linkedin_agent.py
@@ -1,39 +1,50 @@
 from .base_agent import Agent
+from .googleSearch_agent import GoogleSearchAgent
+import os
 
 
-class LinkedinAgent(Agent):
+class LinkedinAgent(GoogleSearchAgent):
     def __init__(self):
         super().__init__("LinkedinAgent")
 
+    
     def description(self) -> str:
         text = """ Cet agent collecte les informations professionnelles 
                 (expériences, compétences, connexions) sur LinkedIn. """
         return text.strip()
 
-    def run(self, params):
-        """
-        Recherche sur LinkedIn pour des informations de profil.
+    def run(self, query):
+        if not query:
+            return {"error": "Aucune requête de recherche n'a été fournie."}
         
-        Args:
-            params (dict): Paramètres nécessaires pour exécuter l'agent.
-                           Par exemple, {'profile_name': 'Abalo Hyppolyte'}
-                           
-        Returns:
-            dict: Informations de profil LinkedIn.
-        """
+        query = "linkedin.com :" + query
 
-        super().run()
+        try:
+            search_params = {"q": query }
+            
+            results = self.client.search(search_params)["inline_images"]
+            print(results) # Retourne une liste
+            # data = []
+            # for result in results:
+            #     tmp = "Title : \"{}\", Source_name : \"{}\", source_link : \"{}\"".format(result["title"], result["source_name"], result["source"])
+            #     data.append(tmp)
+                
+            #     # print(tmp, end="\n\n")
+                
+            #     if len(data) >= 5:
+            #         break
+            
 
-        profile_name = params
-        # Simuler une recherche LinkedIn
-        results = {
-            "profile_name": profile_name,
-            "current_position": "Software Engineer at Company XYZ",
-            "location": "San Francisco, CA",
-            "connections": 150
-        }
-        return results
 
+
+            
+            # return data
+        
+        except Exception as e:
+            return {"error": f"Erreur lors de la recherche Google avec SerpAPI : {str(e)}"}
 
 if __name__ == '__main__':
-    pass
+    import json
+    test = LinkedinAgent(api_key="d615b5a79c503a1cd8a52d9c319e334828fcabd85c5fc67c7814a94bd3069445")
+    answer = test.run("Rosselin Dada")
+    print(answer)
