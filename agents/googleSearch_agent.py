91619cc4aaa93f5a28d14d048665059c5fb892df
Espérance Codjo AYIWAHOUN <eayiwahoun@gmail.com>
2024-10-11 08:36:47 +0100

    Update de serpapi

--- agents/googleSearch_agent.py
+++ agents/googleSearch_agent.py
@@ -1,27 +1,41 @@
 import os
-from serpapi import GoogleSearch
+import serpapi
 from base_agent import Agent
+
+client = serpapi.Client(api_key=os.getenv("API_KEY"))
+results = client.search({
+    'engine': 'bing',
+    'q': 'coffee',
+})
+
 
 class GoogleSearchAgent(Agent):
     def __init__(self, api_key=None):
+        
+        try : 
+            self.client = serpapi.Client(api_key=os.getenv("API_KEY") or api_key)
+        except Exception as e:
+            raise e
+        #     if not self.api_key:
+        #     raise ValueError("Clé API SerpAPI manquante. Veuillez fournir une clé ou définir 'SERP_API_KEY' dans les variables d'environnement.")
+        
         super().__init__("GoogleSearchAgent")
-        self.api_key = api_key or os.getenv("SERPAPI_KEY")
-        if not self.api_key:
-            raise ValueError("Clé API SerpAPI manquante. Veuillez fournir une clé ou définir 'SERPAPI_KEY' dans les variables d'environnement.")
-
+    
     def description(self):
         return "Effectue des recherches Google avancées via SerpAPI."
 
-    def run(self, params):
-        query = params.get("query", "")
+    def run(self, query):
         if not query:
             return {"error": "Aucune requête de recherche n'a été fournie."}
 
         try:
-            search_params = {"q": query, "api_key": self.api_key, "num": 10}
-            search = GoogleSearch(search_params)
-            results = search.get_dict()
-
+            search_params = {"q": query, 
+                             "num": 10 }
+            
+            results = self.client.search(search_params)
+            print(results)
+            return 
+        
             google_results = []
             if "organic_results" in results:
                 for result in results["organic_results"]:
@@ -37,4 +51,6 @@ class GoogleSearchAgent(Agent):
             return {"error": f"Erreur lors de la recherche Google avec SerpAPI : {str(e)}"}
 
 if __name__ == '__main__':
-    pass
+    test = GoogleSearchAgent(api_key="d615b5a79c503a1cd8a52d9c319e334828fcabd85c5fc67c7814a94bd3069445")
+    answer = test.run("Benjamin Tardy")
+    print(answer)
